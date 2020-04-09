8757c641d9650f49cbc32ef2ac37c4e49a2e7777
NeverBehave <gayhub@never.pet>
2020-04-08 22:14:03 -0400

    hw5 revised fileio

--- src/test/java/hw5/MarvelPathsTest.java
+++ src/test/java/hw5/MarvelPathsTest.java
@@ -3,9 +3,9 @@ package hw5;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.stream.Stream;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 import static org.junit.Assert.assertEquals;
 
@@ -19,29 +19,33 @@ public class MarvelPathsTest {
         p.createNewGraph(myOwnCSV);
     }
 
-    private String readFileContent(String filename) {
+    private String readFileContent(String filename) throws IOException {
 
-        StringBuilder contentBuilder = new StringBuilder();
-        try (Stream<String> stream = Files.lines(Paths.get(filename))) {
-            stream.forEach(s -> contentBuilder.append(s).append("\n"));
-        } catch (Exception e) {
-            System.out.println("An error occurred when reading file " + filename);
-            e.printStackTrace();
+        BufferedReader reader = new BufferedReader(new FileReader(filename));
+        String line;
+        StringBuilder result = new StringBuilder();
+
+        while ((line = reader.readLine()) != null) {
+            result.append(line).append('\n');
         }
 
-        return contentBuilder.toString();
+        return result.toString();
     }
 
     @Test
     public void findPath() {
-        assertEquals(readFileContent("data/result/char1_char2.txt"), p.findPath("Char1", "Char2"));
-        assertEquals(readFileContent("data/result/char2_char2.txt"), p.findPath("Char2", "Char2"));
-        // Not Exist node/path
-        assertEquals(readFileContent("data/result/char_not_exist.txt"), p.findPath("CharNotExist", "CharNotExist1"));
-        assertEquals(readFileContent("data/result/char1_char_not_exist.txt"), p.findPath("Char1", "CharNotExist"));
-        assertEquals(readFileContent("data/result/char4_no_path.txt"), p.findPath("Char4", "Char1"));
-        // Shortest Path
-        assertEquals(readFileContent("data/result/char1_char7.txt"), p.findPath("Char1", "Char7"));
-        assertEquals(readFileContent("data/result/char7_char1.txt"), p.findPath("Char7", "Char1"));
+        try {
+            assertEquals(readFileContent("data/result/char1_char2.txt"), p.findPath("Char1", "Char2"));
+            assertEquals(readFileContent("data/result/char2_char2.txt"), p.findPath("Char2", "Char2"));
+            // Not Exist node/path
+            assertEquals(readFileContent("data/result/char_not_exist.txt"), p.findPath("CharNotExist", "CharNotExist1"));
+            assertEquals(readFileContent("data/result/char1_char_not_exist.txt"), p.findPath("Char1", "CharNotExist"));
+            assertEquals(readFileContent("data/result/char4_no_path.txt"), p.findPath("Char4", "Char1"));
+            // Shortest Path
+            assertEquals(readFileContent("data/result/char1_char7.txt"), p.findPath("Char1", "Char7"));
+            assertEquals(readFileContent("data/result/char7_char1.txt"), p.findPath("Char7", "Char1"));
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
     }
 }
